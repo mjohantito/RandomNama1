f9bada67ec06dbf0794ccd27b23a8872254cd5e3
Seo Dal-Mi <mjohantito@student.ciputra.ac.id>
2022-03-29 01:09:04 +0700

    Add project files.

---
+++ RandomNama/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace RandomNama
+{
+    partial class RandomNama
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Random = new System.Windows.Forms.Button();
+            this.dgvNama = new System.Windows.Forms.DataGridView();
+            this.labelNama = new System.Windows.Forms.Label();
+            this.labelCustID = new System.Windows.Forms.Label();
+            this.labelAdress = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNama)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Random
+            // 
+            this.Random.Location = new System.Drawing.Point(85, 68);
+            this.Random.Name = "Random";
+            this.Random.Size = new System.Drawing.Size(75, 23);
+            this.Random.TabIndex = 0;
+            this.Random.Text = "Random";
+            this.Random.UseVisualStyleBackColor = true;
+            this.Random.Click += new System.EventHandler(this.Random_Click);
+            // 
+            // dgvNama
+            // 
+            this.dgvNama.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNama.Location = new System.Drawing.Point(85, 156);
+            this.dgvNama.Name = "dgvNama";
+            this.dgvNama.Size = new System.Drawing.Size(546, 258);
+            this.dgvNama.TabIndex = 1;
+            // 
+            // labelNama
+            // 
+            this.labelNama.AutoSize = true;
+            this.labelNama.Location = new System.Drawing.Point(338, 77);
+            this.labelNama.Name = "labelNama";
+            this.labelNama.Size = new System.Drawing.Size(35, 13);
+            this.labelNama.TabIndex = 2;
+            this.labelNama.Text = "label1";
+            // 
+            // labelCustID
+            // 
+            this.labelCustID.AutoSize = true;
+            this.labelCustID.Location = new System.Drawing.Point(341, 42);
+            this.labelCustID.Name = "labelCustID";
+            this.labelCustID.Size = new System.Drawing.Size(35, 13);
+            this.labelCustID.TabIndex = 3;
+            this.labelCustID.Text = "label1";
+            // 
+            // labelAdress
+            // 
+            this.labelAdress.AutoSize = true;
+            this.labelAdress.Location = new System.Drawing.Point(341, 111);
+            this.labelAdress.Name = "labelAdress";
+            this.labelAdress.Size = new System.Drawing.Size(35, 13);
+            this.labelAdress.TabIndex = 4;
+            this.labelAdress.Text = "label1";
+            // 
+            // RandomNama
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelAdress);
+            this.Controls.Add(this.labelCustID);
+            this.Controls.Add(this.labelNama);
+            this.Controls.Add(this.dgvNama);
+            this.Controls.Add(this.Random);
+            this.Name = "RandomNama";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNama)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Random;
+        private System.Windows.Forms.DataGridView dgvNama;
+        private System.Windows.Forms.Label labelNama;
+        private System.Windows.Forms.Label labelCustID;
+        private System.Windows.Forms.Label labelAdress;
+    }
+}
+
---
+++ RandomNama/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace RandomNama
+{
+    public partial class RandomNama : Form
+    {
+       
+
+        MySqlConnection sqlConnect;
+        MySqlCommand sqlCommand;
+        MySqlDataAdapter sqlAdapter;
+        string connectString = "server=139.255.11.84;uid=student;pwd=isbmantap;database=DW_TITO_OLTP1;";
+        string sqlQuery;
+
+        string[] NamaDepan = { "Yohan", "Cliffton", "Billy","Agustian", "Anthony", "Soenarto", "Jono", "Kasino", "Briaan", "Laura","Edbert", "Chyntia", "Fujian", "Ria", "Agus", "Bambang" };
+        string[] LastName = { "Ana", "Keke", "Wijaya", "Bingtoyo", "Lukas", "Tahir", "Jonathan", "Salim", "Christian", "Liadi", "Johito", "Gracia", "Akmal", "Inez", "Nicholas", "Olivia" };
+
+        string[] Address = { "Surabaya", "Gresik", "Kediri", "Jember", "Banyuwangi", "Babatan", "Kaliwates", "Dolly", "Sukoharjo", "Sukomanuggal", "Sukabumi", "Dlanggu", "Sleman", "Jogja", "Lamongan", "Bali", "Kalikuro","BSD", "Bintaro", "Tangerang", "Gencong", "Tretes", "Pacet", "Trawas", "Malang", "Batu", "Lawang", "Situbondo", "Puger", "Pati", "Kudus", "Singosari", "Gununganyar", "Blitar"};
+
+        public RandomNama()
+        {
+            InitializeComponent();
+        }
+
+        private void Random_Click(object sender, EventArgs e)
+        {
+            var rand = new Random();
+
+            int index = rand.Next(NamaDepan.Length);
+            
+
+            int index2 = rand.Next(LastName.Length);
+            labelNama.Text = (NamaDepan[index] + " " + LastName[index2]);
+
+
+            DataTable dtNama = new DataTable();
+
+            string hurufawal;
+            hurufawal = labelNama.Text.Substring(0,1);
+
+            labelCustID.Text = hurufawal;
+
+            int index3 = rand.Next(Address.Length);
+            labelAdress.Text = Address[index3];
+
+        }
+    }
+}
